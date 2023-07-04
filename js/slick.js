2445c7a22ebc0e976d4a0d5751c1000350f2b179
Olena Sydorova <olena.sydorova@gmail.com>
2023-07-04 18:15:27 +0200

    add slick

--- js/slick.js
+++ js/slick.js
@@ -27,3 +27,22 @@ $('.responsive').slick({
     // instead of a settings object
   ],
 });
+
+// http://kenwheeler.github.io/slick/
+
+// 1. Створити у папці js окремий файл, наприклад, "slick.js" і вставити в нього код, що на 1-9 рядках цього файлу
+
+// 2. Підключити його до index.js
+
+// 3. Підключити у head наступні лінки
+//    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
+//    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css" />
+
+// 4. Підключити перед закриваючим тегом </body> наступні лінки
+//    <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+//    <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+//    <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+
+// 5. Задати клас "responsive" на батьківський елемент (div) та огорнути необхідні елементи/картки у теги <div></div>.
+
+//Після цього, при кликі на пагінацію чи перетягування мишкою, картки будуть гортатися
