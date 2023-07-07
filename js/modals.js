0a82be04bf9190053cd0d3d68231366d796ac147
Olena Sydorova <olena.sydorova@gmail.com>
2023-07-07 17:56:22 +0200

    fix modal window

--- js/modals.js
+++ js/modals.js
@@ -1,40 +1,146 @@
-$('.js-open-modal').click(function () {
-  const modalName = $(this).attr('data-modal');
-  const modal = $('.js-modal[data-modal="' + modalName + '"]');
-  modal.addClass('is-show');
-  $('.js-modal-overlay').addClass('is-show');
-  togglePageScrolling();
+// $('.js-open-modal').click(function () {
+//   const modalName = $(this).attr('data-modal');
+//   const modal = $('.js-modal[data-modal="' + modalName + '"]');
+//   modal.addClass('is-show');
+//   $('.js-modal-overlay').addClass('is-show');
+//   togglePageScrolling();
 
-  $('.js-modal-close').click(function () {
-    $(this).parent('.js-modal').removeClass('is-show');
-    $('.js-modal-overlay').removeClass('is-show');
-    togglePageScrolling();
-  });
+//   $('.js-modal-close').click(function () {
+//     $(this).parent('.js-modal').removeClass('is-show');
+//     $('.js-modal-overlay').removeClass('is-show');
+//     togglePageScrolling();
+//   });
 
-  $('.js-modal-overlay').click(function () {
-    $('.js-modal.is-show').removeClass('is-show');
-    $(this).removeClass('is-show');
-    togglePageScrolling();
-  });
-});
+//   $('.js-modal-overlay').click(function () {
+//     $('.js-modal.is-show').removeClass('is-show');
+//     $(this).removeClass('is-show');
+//     togglePageScrolling();
+//   });
+// });
 
-function togglePageScrolling() {
-  if ($('.js-modal-overlay').hasClass('is-show')) {
-    document.body.style.overflow = 'hidden';
-  } else {
-    document.body.style.overflow = 'visible';
-  }
-}
-const modalThanks = document.querySelector('.modal-form-send');
+// function togglePageScrolling() {
+//   if ($('.js-modal-overlay').hasClass('is-show')) {
+//     document.body.style.overflow = 'hidden';
+//   } else {
+//     document.body.style.overflow = 'visible';
+//   }
+// }
+// const modalThanks = document.querySelector('.modal-form-send');
 
-// modalThanks.addEventListener('click', () => {
+// $('.js-open-modal[data-modal="thanks"]').click(function () {
+//   // Закрити модальне вікно 1
 //   $('.js-modal[data-modal="submit"]').removeClass('is-show');
 //   $('.js-modal-overlay').addClass('is-show');
+//   // Відкрити модальне вікно 3
+//   $('.js-modal[data-modal="thanks"]').addClass('is-show');
 // });
-$('.js-open-modal[data-modal="thanks"]').click(function () {
-  // Закрити модальне вікно 1
-  $('.js-modal[data-modal="submit"]').removeClass('is-show');
-  $('.js-modal-overlay').addClass('is-show');
-  // Відкрити модальне вікно 3
-  $('.js-modal[data-modal="thanks"]').addClass('is-show');
+!(function (e) {
+  'function' != typeof e.matches &&
+    (e.matches =
+      e.msMatchesSelector ||
+      e.mozMatchesSelector ||
+      e.webkitMatchesSelector ||
+      function (e) {
+        for (
+          var t = this,
+            o = (t.document || t.ownerDocument).querySelectorAll(e),
+            n = 0;
+          o[n] && o[n] !== t;
+
+        )
+          ++n;
+        return Boolean(o[n]);
+      }),
+    'function' != typeof e.closest &&
+      (e.closest = function (e) {
+        for (var t = this; t && 1 === t.nodeType; ) {
+          if (t.matches(e)) return t;
+          t = t.parentNode;
+        }
+        return null;
+      });
+})(window.Element.prototype);
+
+document.addEventListener('DOMContentLoaded', function () {
+  const modalButtons = document.querySelectorAll('.js-open-modal'),
+    overlay = document.querySelector('.js-backdrop-modal'),
+    closeButtons = document.querySelectorAll('.js-modal-close');
+
+  /* Перебираем массив кнопок */
+  modalButtons.forEach(function (item) {
+    /* Назначаем каждой кнопке обработчик клика */
+    item.addEventListener('click', function (e) {
+      const form = document.querySelector('.modal-form');
+      /* Предотвращаем стандартное действие элемента. Так как кнопку разные
+            люди могут сделать по-разному. Кто-то сделает ссылку, кто-то кнопку.
+            Нужно подстраховаться. */
+      e.preventDefault();
+      document.body.classList.add('modal-open');
+      /* При каждом клике на кнопку мы будем забирать содержимое атрибута data-modal
+            и будем искать модальное окно с таким же атрибутом. */
+      const modalId = this.getAttribute('data-modal'),
+        modalElem = document.querySelector(
+          '.modal[data-modal="' + modalId + '"]'
+        );
+
+      /* После того как нашли нужное модальное окно, добавим классы
+            подложке и окну чтобы показать их. */
+      modalElem.classList.add('active');
+      overlay.classList.add('active');
+
+      form.reset();
+    }); // end click
+  }); // end foreach
+
+  closeButtons.forEach(function (item) {
+    item.addEventListener('click', function (e) {
+      const parentModal = this.closest('.modal');
+      document.body.classList.remove('modal-open');
+      parentModal.classList.remove('active');
+      overlay.classList.remove('active');
+    });
+  }); // end foreach
+
+  document.body.addEventListener(
+    'keyup',
+    function (e) {
+      const key = e.keyCode;
+
+      if (key == 27) {
+        document.querySelector('.modal.active').classList.remove('active');
+        document.querySelector('.js-backdrop-modal').classList.remove('active');
+        document.body.classList.remove('modal-open');
+      }
+    },
+    false
+  );
+
+  overlay.addEventListener('click', function (e) {
+    if (e.target !== e.currentTarget) {
+      return;
+    }
+    document.querySelector('.modal.active').classList.remove('active');
+    this.classList.remove('active');
+    document.body.classList.remove('modal-open');
+  });
+
+  const sendButton = document.querySelector('.modal-form-send');
+  const modal1 = document.querySelector('.modal1');
+  function closeModal() {
+    modal1.classList.remove('active');
+  }
+  sendButton.addEventListener('click', closeModal);
 });
+
+// end ready
+
+// 1. На кнопку відкриття модалки доати клас js-open-modal, атрибут data-modal="1" (1 це номер модалки, значення довільне)
+
+// 2. В кінці боді створити один спільний бекдроп  з класом js-overlay-modal, в який вкласти усі модалки
+
+// 3. На кнопки закриття модалок повішати клас js-modal-close
+
+// 4. На саму модалку задати клас modal,
+// 	атрибут data-modal="1" (де 1 це довільне значення, але має співпадати з таким самим атрибутом на кнопці, яка відкривала цю модалку)
+
+// При відкритті модалки на неї і на бекдроп буде додаватися клас active, тому треба відповідні стилі додати на модалку і бекдроп. Є клас active, вони видимі, немає -- невидимі
